2a06337bff77569d38b04c96bbed2e66980d896a
arul <153939531+Arul3011@users.noreply.github.com>
2024-12-20 04:21:20 +0000

    add

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import { IoReorderThreeOutline } from "react-icons/io5";
 const Nav = () => {
+   const dialogRef = useRef(null);
+
+    // Function to open the dialog
+    const openDialog = () => {
+        if (dialogRef.current) {
+            dialogRef.current.showModal();
+        }
+    };
+
+    // Function to close the dialog
+    const closeDialog = () => {
+        if (dialogRef.current) {
+            dialogRef.current.close();
+        }
   return (
     <div className="nav">
       <h3>Arul.dev</h3>
       <div className="navic">
+        <span>
         <IoReorderThreeOutline />
+        </span>
       </div>
       <div className="list">
         <a href="#home">HOME</a>
@@ -13,6 +29,11 @@ const Nav = () => {
         <a href="#projects">PROJECTS</a>
         <a href="#contect">CONTACT</a>
       </div>
+      <dialog ref={dialogRef}>
+                <h2>Dialog Title</h2>
+                <p>This is a dialog box.</p>
+                <button onClick={closeDialog}>Close</button>
+            </dialog>
     </div>
   );
 };
