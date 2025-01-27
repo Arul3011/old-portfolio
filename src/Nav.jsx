0426164093db8ca1a2ebfcc9edf5d8450afa2c21
arul <153939531+Arul3011@users.noreply.github.com>
2025-01-27 14:28:20 +0000

    correct : animation

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -4,16 +4,18 @@ import './nav.css';
 
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
+  const [stval, setStval] = useState("");
 
   const toggleMobileMenu = () => {
     setIsMobile(!isMobile);
+    setStval(isMobile ? "none" : "");
   };
  
 
   return (
     <nav className="navbar">
       <div className="logo">Arul.dev</div>
-      <ul className={`nav-links ${isMobile ? 'mobile' : 'none'}`}>
+      <ul className={`nav-links ${isMobile ? 'mobile' : stval}`}>
         <li><a href="/#home" onClick={toggleMobileMenu}>Home</a></li>
         <li><a href="/#about" onClick={toggleMobileMenu}>About</a></li>
         <li><a href="/#projects" onClick={toggleMobileMenu}>Projects</a></li>
